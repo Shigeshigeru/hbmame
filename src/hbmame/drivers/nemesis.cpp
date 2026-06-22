1fda8729b8475675f346f6e6610c5ff339a3da53
Shigeshigeru <103879054+Shigeshigeru@users.noreply.github.com>
2026-06-22 23:19:17 +0900

    Update nemesis.cpp
    spclone fix?

--- src/hbmame/drivers/nemesis.cpp
+++ src/hbmame/drivers/nemesis.cpp
@@ -149,8 +149,8 @@ ROM_START( spclonec )
 	ROM_LOAD( "587-c01.10a",   0x00000, 0x20000, CRC(09fe0632) SHA1(4c3b29c623d70bbe8a938a0beb4638912c46fb6a) )
 ROM_END
 
-GAME( 2007, spclone,  salamand, salamand, lifefrcj, salamand_state, empty_init, ROT0, "Konami", "Salamander (version YY 0.7.3) ", MACHINE_SUPPORTS_SAVE )
-GAME( 2007, spcloneo, salamand, salamand, lifefrcj, salamand_state, empty_init, ROT0, "Konami", "Salamander (version YY 0.7.2.2)", MACHINE_SUPPORTS_SAVE )
-GAME( 2007, spclonec, salamand, salamand, lifefrcj, salamand_state, empty_init, ROT0, "Konami", "Salamander (version YY 0.7.3 unkown Chimera?)", MACHINE_SUPPORTS_SAVE )
+GAME( 2007, spclone,  salamand, salamand, spclone, salamand_state, empty_init, ROT0, "Konami", "Salamander (version YY 0.7.3) ", MACHINE_SUPPORTS_SAVE )
+GAME( 2007, spcloneo, salamand, salamand, spclone, salamand_state, empty_init, ROT0, "Konami", "Salamander (version YY 0.7.2.2)", MACHINE_SUPPORTS_SAVE )
+GAME( 2007, spclonec, salamand, salamand, spclone, salamand_state, empty_init, ROT0, "Konami", "Salamander (version YY 0.7.3 unkown Chimera?)", MACHINE_SUPPORTS_SAVE )
 
 
